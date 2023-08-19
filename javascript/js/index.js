3e4b382dce63d77ce78301d89902625f4d901f37
venkatch669 <ch.venkat669@gmail.com>
2023-08-19 13:37:46 +0530

    html interview points and datatypes of javascript

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,6 +1,12 @@
 var a=10;
 var b=true;
-var c="venkat";
+var c="venkat122";
 console.log(a);
 console.warn(b);
 console.error(c);
+
+var obj1={name:"mohan"};
+var obj2=obj1;
+obj2.name="rajesh";
+console.log(obj1);  // mohan 
+console.log(obj2);  // rajesh
