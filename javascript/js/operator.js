4ab5ad64d051e990884553d0c00cef9cbbb43f86
venkatch669 <ch.venkat669@gmail.com>
2023-08-20 08:40:32 +0530

    github and operators

---
+++ javascript/js/operator.js
@@ -0,0 +1,15 @@
+var a=10;
+var b=20;
+var c=20;
+var d='20';
+var e=20
+e*=10; // e= e*10; // 
+var dd = a>b ? a : b
+console.log("ddd", dd);
+
+console.log("&&", true && false);
+console.log("||", true || false);
+console.log("!", !true);
+
+console.log(100 && 100);
+console.log( false  || 10)
