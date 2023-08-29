0b0ce6da49b078ed288bb32d0488787d2f9f0a72
venkatch669 <ch.venkat669@gmail.com>
2023-08-29 08:39:26 +0530

    functional programming and notes

--- javascript/js/functional-programming.js
+++ javascript/js/functional-programming.js
@@ -17,11 +17,52 @@
 
 // console.log(a); 10,20,30
 
-function demoFun(a){
-    var num=10;
-    return a(num);
-}
+// function demoFun(a){
+//     var num=10;
+//     return a(num);
+// }
 
-demoFun(function(n){
-    console.log(n);
-})	
+// demoFun(function(n){
+//     console.log(n);
+// })	
+
+// impure function
+// var a=[];
+// function append(val){
+//     a.push(val);
+//     return a;
+// }
+// var result = append(10);
+// append(20);
+// append(30);
+// console.log("result", result)
+// console.log("orginal", a)
+
+// pure function 
+// var a=[];
+// function append(val){
+//     var b=a.concat(val);
+//     return b;
+// }
+// append(10);
+// append(20);
+// var result = append(30);
+// console.log("result", result)
+// console.log("orginal", a)
+
+// Clousure 
+function getObj(value){
+    var initalValue =value;
+    function increment(){
+        return initalValue++;
+    }
+    function decrement() {
+        return initalValue--;
+    }
+    return {increment, decrement}							 
+ }
+
+var result = getObj(10);
+console.log(result.decrement());
+console.log(result.decrement());
+console.log(result.decrement());
