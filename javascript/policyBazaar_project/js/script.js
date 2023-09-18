1b2bdfc9628c82b30530e13b77ab28d1dbfb9c2e
venkatch669 <ch.venkat669@gmail.com>
2023-09-18 08:39:03 +0530

    policybazzar_step2-implemenation

--- javascript/policyBazaar_project/js/script.js
+++ javascript/policyBazaar_project/js/script.js
@@ -1,3 +1,5 @@
+
+let selectedValueFromStep1=[];
 const obj=[
     {
         name:'Self',
@@ -41,23 +43,81 @@ const obj=[
     }
 ]
 
+const prepareList = (no)=>{
+    let items = ''
+    for(let i=0;i<no;i++){
+        items +=`<li><button class="btn btn-outline-primary btn-block"><img src="${obj[i].pathURL}"/>${obj[i].name}</button></li>`
+    }
+    $("#items").html(items); // html will replace selector 
+    // $("#items").append(items); // append will append to the selector
+}   
+
+let prepareDropdowns = function(){
+    let val =''
+    for(let i=0;i<selectedValueFromStep1.length;i++){
+        val+=`<li>
+        <div class="row">
+        <div class="col-2">                                  
+                <img src="./images/health-self-icon.svg"/>
+        </div>
+        <div class="col-10">
+            <select class="form-select" aria-label="Default select example">
+            ${getDropdownValues()}
+            </select>
+        </div>
+    </li>`
+    }
+    $("#dropdowns").html(val);    
+}
+let getDropdownValues = () =>{
+    let options ='';
+    for(let i=18;i<=100;i++){
+        options += `<option>${i} Years</option>`
+    }
+    return options;
+}
+
+// $("#items .btn-block").click(()=>{
+//     // $(this).toggleClass("selected");
+//     alert("Test")
+// });
+
 $(document).ready(function(){
     prepareList(6);
 });
 
-const prepareList = (no)=>{
-    let items = ''
-    for(let i=0;i<no;i++){
-        items +=`<li>
-                <button class="btn btn-outline-primary btn-block">
-                    <img src="${obj[i].pathURL}"/>
-                    ${obj[i].name}
-                </button>
-        </li>`
-    }
-    $("#items").html(items);
-}
-
 $("#moreoptions").click(()=>{
     prepareList(obj.length);
+})
+
+$(document).on("click","#items .btn-block",function(){
+    $(this).toggleClass("selected-btn");
+    console.log(this);
+    let text= $(this).text();
+    let index = selectedValueFromStep1.indexOf(text);
+    if(index==-1){
+        selectedValueFromStep1.push(text)
+    }
+    else{
+        selectedValueFromStep1.splice(index,1);
+    }
+    console.log(selectedValueFromStep1);
+    $("#step-1-error-msg").hide();
+    // alert("Test")
+})
+
+$("#step1-continue").click(()=>{
+    if(selectedValueFromStep1.length){
+        $("#step-1").hide();
+        $("#step-2").show();
+       
+        prepareDropdowns();
+    }
+    else{
+        $("#step-1-error-msg").show();
+    }
+})
+$("#step2-back").click(()=>{
+    $("#step-1").show();
+    $("#step-2").hide();
 })
