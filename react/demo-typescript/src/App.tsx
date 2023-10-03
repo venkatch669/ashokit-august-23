f74df79848ebe1ee9835dd4a4cdc3a7b2d9d835f
venkatch669 <ch.venkat669@gmail.com>
2023-10-03 08:58:03 +0530

    folder structure and material ui

---
+++ react/demo-typescript/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+function App() {
+  return (
+    <div className="App">
+       <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
