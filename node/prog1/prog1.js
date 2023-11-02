624f916d36928096516428e0edecdcb4fc2155c3
venkatch669 <ch.venkat669@gmail.com>
2023-11-02 08:38:43 +0530

    node js first prog

---
+++ node/prog1/prog1.js
@@ -0,0 +1,6 @@
+function getMesg(i){
+console.log("welcome to NodeJS" + i);
+}
+for(var i=0;i<10;i++){
+    getMesg(i)
+}
---
+++ node/prog1/prog2.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const data =[
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    }
+];
+
+fs.writeFile("./demo.json", JSON.stringify(data),(err)=>{
+    if(err){
+        console.log("something went wrong,please try again")
+    }
+    else{
+        console.log("file created, successfully");
+    }
+})
+
+fs.readFile("./demo.json",'utf8',(err,res)=>{
+    if(err){
+        console.log("no file found");
+    }else{
+        console.log(res);
+    }
+});
